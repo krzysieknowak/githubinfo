74c06e1d0d53104b4520a8231fdea41c9b7b512c
Krzysztof Nowak <krzyqq@gmail.com>
2024-03-18 15:09:57 +0100

    Initial commit

---
+++ src/test/java/com/knowak/githubinfo/GithubinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowak.githubinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
