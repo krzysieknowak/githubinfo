d1acaf78068195c25ea4d4e581bb891cbd38d4eb
Krzysztof Nowak <krzyqq@gmail.com>
2024-03-25 16:25:06 +0100

    refactored MediaTypeErrorHandler

--- src/main/java/com/knowak/githubinfo/mediaerror/MediaTypeErrorHandler.java
+++ src/main/java/com/knowak/githubinfo/mediaerror/MediaTypeErrorHandler.java
@@ -5,17 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Log4j2
 public class MediaTypeErrorHandler {
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ResponseEntity<MediaTypeErrorResponseDto> handleMediaTypeError(HttpMediaTypeNotAcceptableException exception) {
         String message = exception.getMessage();
         MediaTypeErrorResponseDto responseDto = new MediaTypeErrorResponseDto(HttpStatus.NOT_ACCEPTABLE.value(), message);
